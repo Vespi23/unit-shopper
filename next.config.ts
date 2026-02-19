b8432da28b2a9fdd2b4629b8fe7c88410f85d4c6
vespi23 <successinventor511@gmail.com>
2026-02-19 16:59:26 -0500

    fix: Remove deprecated Sentry disableLogger option

--- next.config.ts
+++ next.config.ts
@@ -101,5 +101,5 @@ export default withSentryConfig(nextConfig, {
   tunnelRoute: "/monitoring",
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  // disableLogger: true,
 });
