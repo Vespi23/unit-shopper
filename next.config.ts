321dc64e8196d6af93905e470b694abd2e187a77
vespi23 <successinventor511@gmail.com>
2026-03-11 00:03:06 -0400

    Allow extension to embed BudgetLynx via iframe

--- next.config.ts
+++ next.config.ts
@@ -18,10 +18,7 @@ const nextConfig: NextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block'
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
+
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
